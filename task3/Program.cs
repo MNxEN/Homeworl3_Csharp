eef6b27f76f0d83e7399174cc73b516fe356ca8c
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-20 01:08:31 +0500

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите число ->");
+
+for (int digit = 1; digit <= number; digit++)
+{
+    int result = digit * digit * digit;
+    Console.Write($" {result} ");
+}
+
+/*
+int digit = 1;
+while (digit <= number)
+{
+    int result = digit * digit * digit;
+    Console.Write($" {result} ");
+    digit++;
+}
+*/
